95cd2accd6ad0fe8152aa0a19005620f927d6ed9
josan <l>
2021-08-30 11:39:05 +0800

    增加日志模块

--- src/main/java/com/yc/ycdemo/YcDemoApplication.java
+++ src/main/java/com/yc/ycdemo/YcDemoApplication.java
@@ -1,14 +1,16 @@
 package com.yc.ycdemo;
 
+import com.yc.ycdemo.util.LogHelper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = "com.yc.ycdemo")
 public class YcDemoApplication {
-
+    private static final String CLASS_NAME = "YcDemoApplication";
     public static void main(String[] args) {
-        System.out.println("启动了");
+        LogHelper.writeInfoLog(CLASS_NAME, "main", "开始启动");
         SpringApplication.run(YcDemoApplication.class, args);
+        LogHelper.writeInfoLog(CLASS_NAME, "main", "启动成功");
     }
 
 }
