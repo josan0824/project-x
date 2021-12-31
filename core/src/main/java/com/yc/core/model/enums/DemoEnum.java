8d2258145932939eee84a4caf63e2d437865aea4
josan <l>
2021-12-31 10:49:51 +0800

    枚举类

---
+++ core/src/main/java/com/yc/core/model/enums/DemoEnum.java
@@ -0,0 +1,21 @@
+package com.yc.core.model.enums;
+
+/**
+ * @author: josan_tang
+ * @create_date: 2021/12/29 13:18
+ * @desc:
+ * @version:
+ */
+public enum DemoEnum {
+    SUCCESS(10, "成功"),
+    FAIL(20, "失败"),
+    RECORDING(30, "录制中");
+
+    public Integer status;
+    public String value;
+
+    DemoEnum(Integer status, String value) {
+        this.status = status;
+        this.value = value;
+    }
+}
