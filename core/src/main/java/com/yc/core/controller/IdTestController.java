535f4680e7b0d04de3d0eebe9ea8e52baa045b74
2287280228@qq.com <kyzg1024>
2022-02-13 12:18:39 +0800

    雪花算法

--- core/src/main/java/com/yc/core/controller/IdTestController.java
+++ core/src/main/java/com/yc/core/controller/IdTestController.java
@@ -1,5 +1,6 @@
 package com.yc.core.controller;
 
+import com.yc.core.utils.SnowflakeIdWorker;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiOperation;
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
 import java.util.UUID;
 
 /**
@@ -22,4 +24,13 @@ public class IdTestController {
     public String getUUID() {
         return UUID.randomUUID().toString();
     }
+
+    @GetMapping("/getSnowflakeId")
+    @ApiOperation(value="得到雪花算法id", notes ="得到雪花算法id")
+    public long getSnowflakeId() {
+        return new SnowflakeIdWorker(24).nextId();
+    }
+
+
+
 }
