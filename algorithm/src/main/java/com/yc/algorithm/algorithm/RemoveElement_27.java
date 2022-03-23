40c21687ac43676de384fadecf85a979234954d1
2287280228@qq.com <kyzg1024>
2022-03-23 21:57:01 +0800

    removeElement

---
+++ algorithm/src/main/java/com/yc/algorithm/algorithm/RemoveElement_27.java
@@ -0,0 +1,24 @@
+package com.yc.algorithm.algorithm;
+
+import java.util.Arrays;
+
+public class RemoveElement_27 {
+    public static void main(String[] args) {
+        int[] arr = {3,2,2,3};
+        removeElement(arr, 3);
+    }
+
+    public static void removeElement(int[] nums, int key) {
+        int k = 0; //k表示key个数
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == key) {
+                //如果当前key
+            }
+        }
+
+
+
+        System.out.println(Arrays.toString(nums));
+    }
+
+}
