d702d755da787b2cc0be1bbda3bb1e04f8afe631
2287280228@qq.com <kyzg1024>
2022-03-23 22:04:05 +0800

    removeElement

--- algorithm/src/main/java/com/yc/algorithm/algorithm/RemoveElement_27.java
+++ algorithm/src/main/java/com/yc/algorithm/algorithm/RemoveElement_27.java
@@ -5,20 +5,28 @@ import java.util.Arrays;
 public class RemoveElement_27 {
     public static void main(String[] args) {
         int[] arr = {3,2,2,3};
-        removeElement(arr, 3);
+        System.out.println(removeElement(arr, 3));
     }
 
-    public static void removeElement(int[] nums, int key) {
+    public static int removeElement(int[] nums, int key) {
         int k = 0; //k表示key个数
+        //把元素移动到前面
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] == key) {
-                //如果当前key
+                //如果当前key==num[i]
+                k++;
+            } else {
+                nums[i - k] = nums[i];
             }
         }
 
+        int[] result = new int[k];
+        for (int i = 0; i < k; i++) {
+            result[i] = nums[i];
+        }
 
-
-        System.out.println(Arrays.toString(nums));
+        System.out.println(Arrays.toString(result));
+        return k;
     }
 
 }
