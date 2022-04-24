24fb7e415c0233430f8399d94591887b873962a1
2287280228@qq.com <2287280228@qq.com>
2022-04-24 12:44:17 +0800

    git测试

---
+++ core/src/main/java/com/yc/core/utils/TestGit.java
@@ -0,0 +1,13 @@
+package com.yc.core.utils;
+
+/**
+ * @author: josan_tang
+ * @create_date: 2022/4/24 12:43
+ * @desc:
+ * @version:
+ */
+public class TestGit {
+    public static void main(String[] args) {
+        //1.master上新增
+    }
+}
