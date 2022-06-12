52a2a9db945433c7e9e5ae3de4501a14f0068cd8
2287280228@qq.com <kyzg1024>
2022-06-12 22:51:54 +0800

    增加LinkedList的测试类

---
+++ java/java-basic/src/main/java/com/yc/javabasic/container/collection/LinkedListTest.java
@@ -0,0 +1,13 @@
+package com.yc.javabasic.container.collection;
+
+import java.util.LinkedList;
+
+public class LinkedListTest {
+    public static void main(String[] args) {
+        LinkedList linkedList = new LinkedList();
+
+        linkedList.add("1");
+
+
+    }
+}
