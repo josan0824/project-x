96cf4f9843eb14b4b4350f738eaea211a92dc4d3
2287280228@qq.com <kyzg1024>
2022-06-12 09:05:48 +0800

    增加VectorTest

---
+++ java/java-basic/src/main/java/com/yc/javabasic/container/collection/VectorTest.java
@@ -0,0 +1,18 @@
+package com.yc.javabasic.container.collection;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Vector;
+
+public class VectorTest {
+    public static void main(String[] args) {
+
+        Vector v1 = new Vector();
+        v1.add("1");
+
+        Vector vector = new Vector(v1);
+
+    }
+
+}
