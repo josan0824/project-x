0ca19a8d2af69cfb5d4ec2d8ba42fb4384f5d6bc
2287280228@qq.com <kyzg1024>
2022-06-15 08:36:39 +0800

    增加ConcurrentHashMap的测试类

---
+++ java/java-basic/src/main/java/com/yc/javabasic/container/map/ConcurrentHashMapTest.java
@@ -0,0 +1,12 @@
+package com.yc.javabasic.container.map;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+public class ConcurrentHashMapTest {
+    public static void main(String[] args) {
+        ConcurrentHashMap concurrentHashMap = new ConcurrentHashMap();
+        //1.put操作
+        concurrentHashMap.put("name", "josan");
+
+    }
+}
