1fe9a60070d019034e66490b2e0c060976127e3e
2287280228@qq.com <kyzg1024>
2022-06-15 08:06:53 +0800

    增加HashMap的测试类

---
+++ java/java-basic/src/main/java/com/yc/javabasic/container/map/HashMapTest.java
@@ -0,0 +1,11 @@
+package com.yc.javabasic.container.map;
+
+import java.util.HashMap;
+
+public class HashMapTest {
+    public static void main(String[] args) {
+        HashMap hashMap = new HashMap();
+        hashMap.put("name", "josan");
+    }
+
+}
