4e76dc6fe9626df5c73afa77ad7f37a337d02009
2287280228@qq.com <2287280228@qq.com>
2022-07-05 18:41:47 +0800

    重新写了三路排序

--- algorithm/src/main/java/com/yc/algorithm/algorithm/commonly/ThreeWayFastRow.java
+++ algorithm/src/main/java/com/yc/algorithm/algorithm/commonly/ThreeWayFastRow.java
@@ -11,8 +11,9 @@ import java.util.Arrays;
 public class ThreeWayFastRow {
 
     public static void main(String[] args) {
-        int[] nums = {2,1,2,1,0,1,0,2,1};
-        threeWayFastRow(nums);
+        int[] nums = {2,1,2,1,0,1,1,2,1};
+        //threeWayFastRow(nums);
+        sort2(nums);
     }
 
     /**
@@ -20,7 +21,7 @@ public class ThreeWayFastRow {
      * 拿元素和1比较，如果
      * 1. 等于1，则位置不需要动,i++
      * 2. 如果小于1，则zero++, 把i和zero交换位置，i++
-     * 3. 如果大于1，则tow--, 并把tow和i交换位置，i++
+     * 3. 如果大于1，则tow--, 并把tow和i交换位置，i不变
      * @param nums
      */
     public static void threeWayFastRow(int nums[]) {
@@ -54,4 +55,31 @@ public class ThreeWayFastRow {
         }
         System.out.println(Arrays.toString(nums));
     }
+
+    public static void sort2(int[] arr) {
+        int zero = -1;  //[0，zero] 表示0的区间，默认-1
+        int two = arr.length;   //[two, arr.length - 1]表示2的区间,默认arr.length
+
+        //如果找到到了tow的位置，右边都是2，则不需要继续排序了，则终止
+        for (int i = 0 ; i < two;) {
+            if (arr[i] == 1) {
+                //如果为1，不用交换数据，i++
+                i++;
+            } else if (arr[i] < 1) {
+                //如果<1,则放左边,交换的肯定是arr[i]=1，所以i++
+                zero ++;
+                int temp = arr[zero];
+                arr[zero] = arr[i];
+                arr[i] = temp;
+            } else if (arr[i] > 1) {
+                //如果>1,则放右边,交换的arr[i]不一定为0或者1，所以i不需要自增，需要继续判断i
+                two --;
+                int temp = arr[two];
+                arr[two] = arr[i];
+                arr[i] = temp;
+            }
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+
 }
