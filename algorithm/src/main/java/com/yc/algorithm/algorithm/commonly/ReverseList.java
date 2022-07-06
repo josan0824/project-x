7264daee2f1a05f5e2cf0ba63f483aa938d84bec
2287280228@qq.com <2287280228@qq.com>
2022-07-06 11:53:17 +0800

    链表反转-头插法

--- algorithm/src/main/java/com/yc/algorithm/algorithm/commonly/ReverseList.java
+++ algorithm/src/main/java/com/yc/algorithm/algorithm/commonly/ReverseList.java
@@ -17,6 +17,11 @@ public class ReverseList {
         }
     }
 
+    /**
+     * 递归实现反转
+     * @param head
+     * @return
+     */
     private static ListNode reverseList(ListNode head) {
         if (head == null || head.next == null) {
             //到达尾节点
@@ -29,14 +34,35 @@ public class ReverseList {
         return newHead;
     }
 
+    /**
+     * 链表反转-头插法
+     * @param head
+     * @return
+     */
+    public static ListNode reverseListByInsertHead(ListNode head) {
+        ListNode newHead = new ListNode(-1);
+        while (head != null) {
+            ListNode next = head.next;
+            //插入到newHead的头部
+            head.next = newHead.next;
+            newHead.next = head;
+            //后移
+            head = next;
+        }
+        return newHead.next;
+    }
+
     public static void main(String[] args) {
         ListNode n1 = new ListNode<>(1);
         ListNode n2 = new ListNode<>(2);
         n1.next = n2;
         ListNode n3 = new ListNode<>(3);
         n2.next = n3;
-        ListNode newHead = reverseList(n1);
+        ListNode newHead = reverseListByInsertHead(n1);
         System.out.println(newHead.value);
 
     }
+
+
+
 }
