c4c82b8eefbbcd4ac223eb4d8d26d61b731ea09b
2287280228@qq.com <2287280228@qq.com>
2022-07-08 19:47:33 +0800

    快排

--- algorithm/src/main/java/com/yc/algorithm/algorithm/commonly/QuickSort.java
+++ algorithm/src/main/java/com/yc/algorithm/algorithm/commonly/QuickSort.java
@@ -1,6 +1,7 @@
 package com.yc.algorithm.algorithm.commonly;
 
 import java.util.Arrays;
+import java.util.Random;
 
 /**
  * @author: josan_tang
@@ -10,13 +11,13 @@ import java.util.Arrays;
  */
 public class QuickSort {
     public static void main(String[] args) {
-        int[] arr = {-9, 78, 0, 0, 1, 0, 3, -1, 23, -56, 7};
+        int[] arr = {6,11, 3,9,8};
         quickSort(arr);
         System.out.println(Arrays.toString(arr));
     }
 
     public static void quickSort(int[] arr){
-        quickSort(arr, 0, arr.length - 1);
+        quickSort2(arr, 0, arr.length - 1);
     }
 
     private static void quickSort(int[] arr, int left, int right){
@@ -42,6 +43,74 @@ public class QuickSort {
         //分别对基准数两端的子数组进行快排
         quickSort(arr, left, i - 1);
         quickSort(arr, i + 1, right);
+    }
+
+
+    /**
+     * 快速排序
+     * @param arr
+     * @param left
+     * @param right
+     */
+    private static void quickSort2(int[] arr, int left, int right) {
+        if (left >= right) {
+            return;
+        }
+        //得到分区的位置
+        int i = partition(arr, left, right);
+        //排序左边的分区
+        quickSort2(arr, left, i- 1);
+        //排序右边的分区
+        quickSort2(arr, i + 1, right);
+    }
+
+    /**
+     * 分区函数
+     * @param arr
+     * @param left
+     * @param right
+     * @return 分区后的位置
+     */
+    private static int partition(int[] arr, int left, int right) {
+        //随机得到主元
+        int pivot = getPivot(arr, left, right);
+        //i表示分区点的位置，则[left, i - 1]表示小于pivot的区间，遍历数组时，发现有小于pivot的值，则跟i交换，并让i++，加入到该区间
+        int i = left;
+        for (int j = left; j < right; j++) {
+            if (arr[j] < pivot) {
+                //交换i和j的位置
+                swap(arr, i , j);
+                i++;
+            }
+        }
+        //最后交换
+        swap(arr, i , right);
+        return i;
+    }
+
+    /**
+     * 获取主元
+     * @param arr
+     * @param left
+     * @param right
+     * @return
+     */
+    public static int getPivot(int[] arr, int left, int right) {
+        int pivot = new Random().nextInt(right - left + 1) + left;
+        swap(arr, pivot, right);
+        return arr[right];
+    }
+
+    /**
+     * 交换
+     * @param arr
+     * @param i
+     * @param j
+     */
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[j];
+        arr[j] = arr[i];
+        arr[i] = temp;
     }
 
 }
