3f6782ddcf0f2d485748b594302bbe52194bc808
2287280228@qq.com <2287280228@qq.com>
2022-07-12 10:46:24 +0800

    redission分布式锁的实现

--- redisutils/src/main/java/com/yc/redis/controller/RedisLockController.java
+++ redisutils/src/main/java/com/yc/redis/controller/RedisLockController.java
@@ -8,6 +8,8 @@ import com.yc.redis.utils.LogHelper;
 import com.yc.redis.utils.RedisUtils;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.redisson.Redisson;
+import org.redisson.api.RLock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -31,26 +33,54 @@ public class RedisLockController {
     @Resource
     private IdWorker idWorker;
 
+    @Resource
+    private Redisson redisson;
+
     @Autowired
     private RedisUtils redisUtils;
 
     @ApiOperation(value="redis分布式锁", notes ="redis分布式锁")
     @GetMapping("/getLock")
-    public String getLock() throws InterruptedException {
-        String lockKey = "user";
+    public String getRedisLock() throws InterruptedException {
+        LogHelper.writeInfoLog(this.getClass().getSimpleName(), "getLock", "进入" +  Thread.currentThread().getName());
+        String lockKey = "getRedisLock";
         String lockValue = String.valueOf(idWorker.nextId());
         long outtime = 60L;
         try {
+            //加锁的key、value、超时时间
             if (redisUtils.addLock(lockKey, lockValue, outtime)) {
-                LogHelper.writeInfoLog(this.getClass().getSimpleName(), "getLock", "获得锁");
+                LogHelper.writeInfoLog(this.getClass().getSimpleName(), "getLock", "获得锁"  + Thread.currentThread().getName());
                 //模拟业务处理
-                Thread.sleep(5000);
+                Thread.sleep(10000);
             } else {
-                LogHelper.writeInfoLog(this.getClass().getSimpleName(), "getLock", "未获取到锁");
+                LogHelper.writeInfoLog(this.getClass().getSimpleName(), "getLock", "未获取到锁"  + Thread.currentThread().getName());
             }
         } finally {
-            LogHelper.writeInfoLog(this.getClass().getSimpleName(), "getLock", "释放锁");
+            LogHelper.writeInfoLog(this.getClass().getSimpleName(), "getLock", "释放锁"  + Thread.currentThread().getName());
             redisUtils.unlock(lockKey, lockValue);
+        }
+        return "ok";
+    }
+
+    @ApiOperation(value="redission分布式锁", notes ="redission分布式锁")
+    @GetMapping("/getRedissionLock")
+    public String getRedissionLock() throws InterruptedException {
+        LogHelper.writeInfoLog(this.getClass().getSimpleName(), "getRedissionLock", "进入" +  Thread.currentThread().getName());
+        String lockKey = "getRedisLock";
+        RLock lock = redisson.getLock(lockKey);
+        try {
+            //等待时间，超时时间，单位
+            if (lock.tryLock(5, 20, TimeUnit.SECONDS)) {
+                LogHelper.writeInfoLog(this.getClass().getSimpleName(), "getRedissionLock", "得到分布式锁" +  Thread.currentThread().getName());
+                Thread.sleep(8000);
+            } else {
+                LogHelper.writeInfoLog(this.getClass().getSimpleName(), "getRedissionLock", "未获得分布式锁" +  Thread.currentThread().getName());
+            }
+        } finally {
+            if (lock.isLocked() && lock.isHeldByCurrentThread()) {
+                LogHelper.writeInfoLog(this.getClass().getSimpleName(), "getRedissionLock", "释放分布式锁" +  Thread.currentThread().getName());
+                lock.unlock();
+            }
         }
         return "ok";
     }
